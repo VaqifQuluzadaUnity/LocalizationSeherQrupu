4f32ee01d14a0ccbc46a5bf9a5f8b746bcd40341
7empestGit <106082341+elka39@users.noreply.github.com>
2022-08-15 12:17:34 +0400

    Localization added

---
+++ LocalizationSeherQrupu/Assets/LocalizationController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+public class LocalizationController : MonoBehaviour
+{
+    public void OnLanguageDropDownPressed(int languageIndex)
+  {
+    Locale selectedLocale = LocalizationSettings.AvailableLocales.Locales[languageIndex];
+
+    LocalizationSettings.Instance.SetSelectedLocale(selectedLocale);
+  }
+}
